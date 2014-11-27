e5f25d669b097bef831f1ee338fa642304c0800d
Jamie <i7218850@bournemouth.ac.uk>
2014-11-27 03:25:33 +0000

    Created a Building class inherits Entity. The class has been Doxygen commented.

---
+++ PGGAssignment1SDL/building.h
@@ -0,0 +1,26 @@
+#pragma once
+#ifndef BUILDING_H
+#define BUILDING_H
+
+#include "entity.h"
+
+/**
+@brief Creates a Building object that inherits Entity.
+*/
+class Building : public Entity
+{
+private:
+public:
+	/**
+	Constructs Building object
+	Constructs a Building object.
+	*/
+	Building();
+
+	/**
+	De-constructs a Building object
+	De-constructs the Building object
+	*/
+	~Building();
+};
+#endif
